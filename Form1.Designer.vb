6ef8462ee94fd46c956bd4015df2f4f161438bf8
michaelchen1225 <90543072+michaelchen1225@users.noreply.github.com>
2022-10-18 20:40:00 +0800

    加入專案檔案。

---
+++ Form1.Designer.vb
@@ -0,0 +1,261 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 覆寫 Dispose 以清除元件清單。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    '為 Windows Form 設計工具的必要項
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下為 Windows Form 設計工具所需的程序
+    '可以使用 Windows Form 設計工具進行修改。
+    '請勿使用程式碼編輯器進行修改。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btn7 = New System.Windows.Forms.Button()
+        Me.btnMulti = New System.Windows.Forms.Button()
+        Me.btn6 = New System.Windows.Forms.Button()
+        Me.btnMunus = New System.Windows.Forms.Button()
+        Me.btnDiv = New System.Windows.Forms.Button()
+        Me.btn3 = New System.Windows.Forms.Button()
+        Me.btnEql = New System.Windows.Forms.Button()
+        Me.btn0 = New System.Windows.Forms.Button()
+        Me.btn2 = New System.Windows.Forms.Button()
+        Me.btnC = New System.Windows.Forms.Button()
+        Me.btn1 = New System.Windows.Forms.Button()
+        Me.btn5 = New System.Windows.Forms.Button()
+        Me.btn4 = New System.Windows.Forms.Button()
+        Me.btnPlus = New System.Windows.Forms.Button()
+        Me.btn9 = New System.Windows.Forms.Button()
+        Me.btn8 = New System.Windows.Forms.Button()
+        Me.lbDisplay = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btn7
+        '
+        Me.btn7.Location = New System.Drawing.Point(10, 111)
+        Me.btn7.Name = "btn7"
+        Me.btn7.Size = New System.Drawing.Size(60, 60)
+        Me.btn7.TabIndex = 13
+        Me.btn7.Text = "7"
+        Me.btn7.UseVisualStyleBackColor = True
+        Me.btn7.UseWaitCursor = True
+        '
+        'btnMulti
+        '
+        Me.btnMulti.Location = New System.Drawing.Point(208, 241)
+        Me.btnMulti.Name = "btnMulti"
+        Me.btnMulti.Size = New System.Drawing.Size(60, 60)
+        Me.btnMulti.TabIndex = 2
+        Me.btnMulti.Text = "*"
+        Me.btnMulti.UseVisualStyleBackColor = True
+        Me.btnMulti.UseWaitCursor = True
+        '
+        'btn6
+        '
+        Me.btn6.Location = New System.Drawing.Point(142, 177)
+        Me.btn6.Name = "btn6"
+        Me.btn6.Size = New System.Drawing.Size(60, 60)
+        Me.btn6.TabIndex = 8
+        Me.btn6.Text = "6"
+        Me.btn6.UseVisualStyleBackColor = True
+        Me.btn6.UseWaitCursor = True
+        '
+        'btnMunus
+        '
+        Me.btnMunus.Location = New System.Drawing.Point(208, 175)
+        Me.btnMunus.Name = "btnMunus"
+        Me.btnMunus.Size = New System.Drawing.Size(60, 60)
+        Me.btnMunus.TabIndex = 1
+        Me.btnMunus.Text = "-"
+        Me.btnMunus.UseVisualStyleBackColor = True
+        Me.btnMunus.UseWaitCursor = True
+        '
+        'btnDiv
+        '
+        Me.btnDiv.Location = New System.Drawing.Point(208, 307)
+        Me.btnDiv.Name = "btnDiv"
+        Me.btnDiv.Size = New System.Drawing.Size(60, 60)
+        Me.btnDiv.TabIndex = 3
+        Me.btnDiv.Text = "/"
+        Me.btnDiv.UseVisualStyleBackColor = True
+        Me.btnDiv.UseWaitCursor = True
+        '
+        'btn3
+        '
+        Me.btn3.Location = New System.Drawing.Point(142, 241)
+        Me.btn3.Name = "btn3"
+        Me.btn3.Size = New System.Drawing.Size(60, 60)
+        Me.btn3.TabIndex = 7
+        Me.btn3.Text = "3"
+        Me.btn3.UseVisualStyleBackColor = True
+        Me.btn3.UseWaitCursor = True
+        '
+        'btnEql
+        '
+        Me.btnEql.Location = New System.Drawing.Point(142, 307)
+        Me.btnEql.Name = "btnEql"
+        Me.btnEql.Size = New System.Drawing.Size(60, 60)
+        Me.btnEql.TabIndex = 4
+        Me.btnEql.Text = "="
+        Me.btnEql.UseVisualStyleBackColor = True
+        Me.btnEql.UseWaitCursor = True
+        '
+        'btn0
+        '
+        Me.btn0.Location = New System.Drawing.Point(76, 307)
+        Me.btn0.Name = "btn0"
+        Me.btn0.Size = New System.Drawing.Size(60, 60)
+        Me.btn0.TabIndex = 14
+        Me.btn0.Text = "0"
+        Me.btn0.UseVisualStyleBackColor = True
+        Me.btn0.UseWaitCursor = True
+        '
+        'btn2
+        '
+        Me.btn2.Location = New System.Drawing.Point(76, 241)
+        Me.btn2.Name = "btn2"
+        Me.btn2.Size = New System.Drawing.Size(60, 60)
+        Me.btn2.TabIndex = 6
+        Me.btn2.Text = "2"
+        Me.btn2.UseVisualStyleBackColor = True
+        Me.btn2.UseWaitCursor = True
+        '
+        'btnC
+        '
+        Me.btnC.Location = New System.Drawing.Point(10, 307)
+        Me.btnC.Name = "btnC"
+        Me.btnC.Size = New System.Drawing.Size(60, 60)
+        Me.btnC.TabIndex = 15
+        Me.btnC.Text = "C"
+        Me.btnC.UseVisualStyleBackColor = True
+        Me.btnC.UseWaitCursor = True
+        '
+        'btn1
+        '
+        Me.btn1.Location = New System.Drawing.Point(10, 241)
+        Me.btn1.Name = "btn1"
+        Me.btn1.Size = New System.Drawing.Size(60, 60)
+        Me.btn1.TabIndex = 5
+        Me.btn1.Text = "1"
+        Me.btn1.UseVisualStyleBackColor = True
+        Me.btn1.UseWaitCursor = True
+        '
+        'btn5
+        '
+        Me.btn5.Location = New System.Drawing.Point(76, 175)
+        Me.btn5.Name = "btn5"
+        Me.btn5.Size = New System.Drawing.Size(60, 60)
+        Me.btn5.TabIndex = 9
+        Me.btn5.Text = "5"
+        Me.btn5.UseVisualStyleBackColor = True
+        Me.btn5.UseWaitCursor = True
+        '
+        'btn4
+        '
+        Me.btn4.Location = New System.Drawing.Point(10, 177)
+        Me.btn4.Name = "btn4"
+        Me.btn4.Size = New System.Drawing.Size(60, 60)
+        Me.btn4.TabIndex = 10
+        Me.btn4.Text = "4"
+        Me.btn4.UseVisualStyleBackColor = True
+        Me.btn4.UseWaitCursor = True
+        '
+        'btnPlus
+        '
+        Me.btnPlus.Location = New System.Drawing.Point(208, 109)
+        Me.btnPlus.Name = "btnPlus"
+        Me.btnPlus.Size = New System.Drawing.Size(60, 60)
+        Me.btnPlus.TabIndex = 0
+        Me.btnPlus.Text = "+"
+        Me.btnPlus.UseVisualStyleBackColor = True
+        Me.btnPlus.UseWaitCursor = True
+        '
+        'btn9
+        '
+        Me.btn9.Location = New System.Drawing.Point(142, 110)
+        Me.btn9.Name = "btn9"
+        Me.btn9.Size = New System.Drawing.Size(60, 60)
+        Me.btn9.TabIndex = 11
+        Me.btn9.Text = "9"
+        Me.btn9.UseVisualStyleBackColor = True
+        Me.btn9.UseWaitCursor = True
+        '
+        'btn8
+        '
+        Me.btn8.Location = New System.Drawing.Point(76, 111)
+        Me.btn8.Name = "btn8"
+        Me.btn8.Size = New System.Drawing.Size(60, 60)
+        Me.btn8.TabIndex = 12
+        Me.btn8.Text = "8"
+        Me.btn8.UseVisualStyleBackColor = True
+        Me.btn8.UseWaitCursor = True
+        '
+        'lbDisplay
+        '
+        Me.lbDisplay.BackColor = System.Drawing.Color.LightSteelBlue
+        Me.lbDisplay.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lbDisplay.Font = New System.Drawing.Font("細明體", 14.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.lbDisplay.ForeColor = System.Drawing.Color.Red
+        Me.lbDisplay.Location = New System.Drawing.Point(7, 38)
+        Me.lbDisplay.Name = "lbDisplay"
+        Me.lbDisplay.Size = New System.Drawing.Size(263, 41)
+        Me.lbDisplay.TabIndex = 16
+        Me.lbDisplay.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 18.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(282, 382)
+        Me.Controls.Add(Me.lbDisplay)
+        Me.Controls.Add(Me.btn8)
+        Me.Controls.Add(Me.btn9)
+        Me.Controls.Add(Me.btnPlus)
+        Me.Controls.Add(Me.btn4)
+        Me.Controls.Add(Me.btn5)
+        Me.Controls.Add(Me.btn1)
+        Me.Controls.Add(Me.btnC)
+        Me.Controls.Add(Me.btn2)
+        Me.Controls.Add(Me.btn0)
+        Me.Controls.Add(Me.btnEql)
+        Me.Controls.Add(Me.btn3)
+        Me.Controls.Add(Me.btnDiv)
+        Me.Controls.Add(Me.btnMunus)
+        Me.Controls.Add(Me.btn6)
+        Me.Controls.Add(Me.btnMulti)
+        Me.Controls.Add(Me.btn7)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents btn7 As Button
+    Friend WithEvents btnMulti As Button
+    Friend WithEvents btn6 As Button
+    Friend WithEvents btnMunus As Button
+    Friend WithEvents btnDiv As Button
+    Friend WithEvents btn3 As Button
+    Friend WithEvents btnEql As Button
+    Friend WithEvents btn0 As Button
+    Friend WithEvents btn2 As Button
+    Friend WithEvents btnC As Button
+    Friend WithEvents btn1 As Button
+    Friend WithEvents btn5 As Button
+    Friend WithEvents btn4 As Button
+    Friend WithEvents btnPlus As Button
+    Friend WithEvents btn9 As Button
+    Friend WithEvents btn8 As Button
+    Friend WithEvents lbDisplay As Label
+End Class
---
+++ Form1.vb
@@ -0,0 +1,173 @@
+﻿Public Class Form1
+
+    Dim num1 As Double '數字1
+    Dim num2 As Double '數字2
+    Dim num3 As Double '數字3 
+    Dim resault As Double '結果
+    Dim stOpetaor As String '算符
+
+    Private Sub btn1_Click(sender As Object, e As EventArgs) Handles btn1.Click
+        lbDisplay.Text = btn1.Text
+        num3 = Val(btn1.Text)
+    End Sub
+
+    Private Sub btn2_Click(sender As Object, e As EventArgs) Handles btn2.Click
+        lbDisplay.Text = btn2.Text
+        num3 = Val(btn2.Text)
+    End Sub
+
+    Private Sub btn3_Click(sender As Object, e As EventArgs) Handles btn3.Click
+        lbDisplay.Text = btn3.Text
+        num3 = Val(btn3.Text)
+    End Sub
+
+    Private Sub btn4_Click(sender As Object, e As EventArgs) Handles btn4.Click
+        lbDisplay.Text = btn4.Text
+        num3 = Val(btn4.Text)
+    End Sub
+
+    Private Sub btn5_Click(sender As Object, e As EventArgs) Handles btn5.Click
+        lbDisplay.Text = btn5.Text
+        num3 = Val(btn5.Text)
+    End Sub
+
+    Private Sub btn6_Click(sender As Object, e As EventArgs) Handles btn6.Click
+        lbDisplay.Text = btn6.Text
+        num3 = Val(btn6.Text)
+    End Sub
+
+    Private Sub btn7_Click(sender As Object, e As EventArgs) Handles btn7.Click
+        lbDisplay.Text = btn7.Text
+        num3 = Val(btn7.Text)
+    End Sub
+
+    Private Sub btn8_Click(sender As Object, e As EventArgs) Handles btn8.Click
+        lbDisplay.Text = btn8.Text
+        num3 = Val(btn8.Text)
+    End Sub
+
+    Private Sub btn9_Click(sender As Object, e As EventArgs) Handles btn9.Click
+        lbDisplay.Text = btn9.Text
+        num3 = Val(btn9.Text)
+    End Sub
+
+    Private Sub btn0_Click(sender As Object, e As EventArgs) Handles btn0.Click
+        lbDisplay.Text = btn0.Text
+        num3 = Val(btn0.Text)
+    End Sub
+
+    Private Sub btnPlus_Click(sender As Object, e As EventArgs) Handles btnPlus.Click
+        num2 = Val(lbDisplay.Text)
+        Select Case stOpetaor
+            Case ""
+                num1 = num2
+            Case "+"
+                num1 = num1 + num2
+                lbDisplay.Text = CStr(num1)
+            Case "-"
+                num1 = num1 - num2
+                lbDisplay.Text = CStr(num1)
+            Case "*"
+                num1 = num1 * num2
+                lbDisplay.Text = CStr(num1)
+            Case "/"
+                num1 = num1 / num2
+                lbDisplay.Text = CStr(num1)
+        End Select
+        stOpetaor = "+"
+        num2 = 0
+    End Sub
+
+    Private Sub btnMunus_Click(sender As Object, e As EventArgs) Handles btnMunus.Click
+        num2 = Val(lbDisplay.Text)
+        Select Case stOpetaor
+            Case ""
+                num1 = num2
+            Case "+"
+                num1 = num1 + num2
+                lbDisplay.Text = CStr(num1)
+            Case "-"
+                num1 = num1 - num2
+                lbDisplay.Text = CStr(num1)
+            Case "*"
+                num1 = num1 * num2
+                lbDisplay.Text = CStr(num1)
+            Case "/"
+                num1 = num1 / num2
+                lbDisplay.Text = CStr(num1)
+        End Select
+        stOpetaor = "-"
+        num2 = 0
+    End Sub
+
+    Private Sub btnMulti_Click(sender As Object, e As EventArgs) Handles btnMulti.Click
+        num2 = Val(lbDisplay.Text)
+        Select Case stOpetaor
+            Case ""
+                num1 = num2
+            Case "+"
+                num1 = num1 + num2
+                lbDisplay.Text = CStr(num1)
+            Case "-"
+                num1 = num1 - num2
+                lbDisplay.Text = CStr(num1)
+            Case "*"
+                num1 = num1 * num2
+                lbDisplay.Text = CStr(num1)
+            Case "/"
+                num1 = num1 / num2
+                lbDisplay.Text = CStr(num1)
+        End Select
+        stOpetaor = "*"
+        num2 = 0
+    End Sub
+
+    Private Sub btnDiv_Click(sender As Object, e As EventArgs) Handles btnDiv.Click
+        num2 = Val(lbDisplay.Text)
+        Select Case stOpetaor
+            Case ""
+                num1 = num2
+            Case "+"
+                num1 = num1 + num2
+                lbDisplay.Text = CStr(num1)
+            Case "-"
+                num1 = num1 - num2
+                lbDisplay.Text = CStr(num1)
+            Case "*"
+                num1 = num1 * num2
+                lbDisplay.Text = CStr(num1)
+            Case "/"
+                num1 = num1 / num2
+                lbDisplay.Text = CStr(num1)
+        End Select
+        stOpetaor = "/"
+        num2 = 0
+    End Sub
+
+    Private Sub btnC_Click(sender As Object, e As EventArgs) Handles btnC.Click
+        lbDisplay.Text = btn0.Text
+        num1 = 0
+        num2 = 0
+        stOpetaor = ""
+        resault = 0
+    End Sub
+
+    Private Sub btnEql_Click(sender As Object, e As EventArgs) Handles btnEql.Click
+        Select Case stOpetaor
+            Case "+"
+                resault = num1 + num3
+                lbDisplay.Text = resault
+            Case "-"
+                resault = num1 - num3
+                lbDisplay.Text = resault
+            Case "*"
+                resault = num1 * num3
+                lbDisplay.Text = resault
+            Case "/"
+                resault = num1 / num3
+                lbDisplay.Text = resault
+        End Select
+        num1 = resault
+
+    End Sub
+End Class
